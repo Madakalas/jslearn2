5463cb2c445a28ce47caff4204dfec743823f31b
Madakalas <siddumadakala2001@gmail.com>
2023-12-21 14:17:24 +0530

    async

---
+++ ES6/Async,await/one.js
@@ -0,0 +1,40 @@
+let Employees=[{id:101,name:'siddu',sal:45000}]
+let create_Employees=(emp)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        let flag = true;
+        if(flag){
+            Employees.push(emp);
+            resolve("Data inserted")
+        }
+        else{
+            reject("failed")
+        }
+
+    },4000)
+})
+}
+let display_Employees=()=>{
+    setTimeout(()=>{
+        let rows=""
+
+        for(emp of  Employees){
+            rows  = rows + `<tr>
+                                <td>${emp.id}</td>
+                                <td>${emp.name}</td>
+                                <td>${emp.sal}</td>
+                            </tr>`
+        }
+        document.getElementById('tbody_Data').innerHTML = rows
+    },2000)
+
+
+}
+let execute =async().create_Employees({id:103,name:'amar',sal:'55000'}
+.then((msg)=>{
+    console.log(msg)
+   await. display_Employees()
+})
+.catch((err)=>{
+    console.log(err)
+}))
--- ES6/Asynchronous/Two.js
+++ ES6/Asynchronous/Two.js
@@ -1,26 +1,27 @@
-let employees=[{'id':101,'ename':'siddu','sal':45000},
-                {'id':102,'ename':'wasid','sal':5500}]
-let create_Employee=(emp,callback)=>{
+let employees=[
+    {id:101,name:"rahul",sal:45000},
+    {id:102,name:"Sonia",sal:55000}
+]
+let create_Employees=(emp,callback)=>{
     setTimeout(()=>{
-         employees.push(emp)
-         callback()
+        employees.push(emp)
+        callback()
     },4000)
 }
 let display_Employees=()=>{
     setTimeout(()=>{
         let rows=""
-        for(emp of employees){
-            rows = rows+`<tr>
-                            <td>${emp.id}</td>
-                            <td>${emp.ename}</td>
-                            <td>${emp.sal}</td>
+
+        for(emp of  employees){
+            rows  = rows + `<tr>
+                                <td>${emp.id}</td>
+                                <td>${emp.name}</td>
+                                <td>${emp.sal}</td>
                             </tr>`
-
         }
-        document.getElementById('tbody_Data').innerHTML=rows
+        document.getElementById('tbody_Data').innerHTML = rows
+    
     },1000)
-}    
+}
 
-
-
-create_Employee=({'id':103,'ename':'amar','sal':55000},display_Employees)
+create_Employees({id:103,name:"Priyanka",sal:65000},display_Employees)
